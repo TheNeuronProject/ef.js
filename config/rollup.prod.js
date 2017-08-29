411fe2aad5a1bfa830027114d96d4aaafddd4a79
Yukino Song <syqlds@126.com>
2017-08-30 01:24:44 +0800

    Update ef-core to v0.4.1-beta.1

--- config/rollup.prod.js
+++ config/rollup.prod.js
@@ -1,7 +1,9 @@
 // Import base config
 import base from './rollup.base'
 
-base.output.file = base.proDest
+if (process.env.BUILD_TARGET === 'production') base.output.file = `${base.proPath}/${base.bundle}.min.js`
+else base.output.file = `${base.proPath}/${base.bundle}.dev.js`
+
 base.output.sourcemap = process.env.BUILD_ENV === 'DEMO' || process.env.BUILD_ENV === 'CI' ? base.output.sourcemap : false
 
 export default base
