93905dfe6cc9c628bfb2b877f706c0783434dca4
Yukino Song <yukino@yuki.network>
2022-09-24 06:39:37 +0800

    Fix linked list x2

--- src/ef.js
+++ src/ef.js
@@ -71,7 +71,7 @@ const setParser = (newParser) => {
  */
 const t = (...args) => create(mixStr(...args))
 
-let coreVersion = '0.15.3'
+let coreVersion = '0.15.5'
 
 if (process.env.NODE_ENV !== 'production') {
 	coreVersion = `${coreVersion}+debug`
